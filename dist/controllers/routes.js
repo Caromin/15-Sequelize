13b0449a9a81664c79ab777a0f9a66ad6ff3752b
Caromin <c.aromin.unf@gmail.com>
2017-10-27 12:51:59 -0400

    used raw: true, and json.stringify to convert the raw data into a workable object

--- dist/controllers/routes.js
+++ dist/controllers/routes.js
@@ -1,6 +1,10 @@
 'use strict';
 
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 // required packages
+var Sequelize = require('sequelize');
+var Op = Sequelize.Op;
 var express = require('express');
 // alternative to app.get where (app) is passed in from the server.js file
 var router = express.Router();
@@ -8,8 +12,8 @@ var models = require('./../models');
 
 router.get("/", function (req, res) {
     //Burger must be capitalized, because it is calling the variable inside of the burger model
-    models.Burger.findAll({ where: { burgerName: 'secondBurger' } }).then(function (found) {
-        console.log(found.burgerName);
+    models.Burger.findAll({ where: { devoured: _defineProperty({}, Op.eq, true) } }).then(function (found) {
+        console.log('this burger was not consumed' + found.burgerName);
     });
 
     res.render("index");
--- src/controllers/routes.js
+++ src/controllers/routes.js
@@ -1,4 +1,6 @@
 // required packages
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 const express = require('express');
 // alternative to app.get where (app) is passed in from the server.js file
 const router = express.Router();
@@ -6,8 +8,15 @@ const models = require('./../models');
 
 router.get("/", function(req, res) {
   //Burger must be capitalized, because it is calling the variable inside of the burger model
-  models.Burger.findOne({ where: {burgerName: 'secondBurger' }}).then(found => {
-  console.log(found.burgerName);
+  //use raw: true to display the data in an array format
+  models.Burger.findAll({ raw: true}).then(found => {
+    console.log('length: ' + found.length);
+    //use a for loop to loop through all of the potential answers in the array generated
+    //from the query above
+    for (i=0; i < found.length; i++) {
+      //use JSON.stringify to covert the data into a workable object
+      console.log('this burger was not consumed: ' + JSON.stringify(found[i].burgerName));
+    }
   })
 
   res.render("index");
