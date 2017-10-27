66367ece939f655614cece003da488e279ea30e2
Caromin <c.aromin.unf@gmail.com>
2017-10-27 13:09:08 -0400

    findAll is working / route is functioning, working on creating now

--- dist/controllers/routes.js
+++ dist/controllers/routes.js
@@ -1,7 +1,5 @@
 'use strict';
 
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
 // required packages
 var Sequelize = require('sequelize');
 var Op = Sequelize.Op;
@@ -11,24 +9,37 @@ var router = express.Router();
 var models = require('./../models');
 
 router.get("/", function (req, res) {
-    //Burger must be capitalized, because it is calling the variable inside of the burger model
-    models.Burger.findAll({ where: { devoured: _defineProperty({}, Op.eq, true) } }).then(function (found) {
-        console.log('this burger was not consumed' + found.burgerName);
-    });
-
-    res.render("index");
+  //Burger must be capitalized, because it is calling the variable inside of the burger model
+  //use raw: true to display the data in an array format
+  models.Burger.findAll({ raw: true }).then(function (found) {
+    // console.log('length: ' + found.length);
+    //create an empty array for objects to be pushed into, to be able to work with views engine
+    var arr = [];
+    //use a for loop to loop through all of the potential answers in the array generated
+    //from the query above
+    for (i = 0; i < found.length; i++) {
+      //use JSON.stringify to covert the data into a workable object
+      // console.log('this burger was not consumed: ' + found[i]);
+      arr.push(found[i]);
+    };
+    //arr will say [object object], that just means an object converted into a string
+    // console.log('this is arr: ' + arr);
+    //the PURPOSE OF arr is when the data is called in views it will be called in a each loop
+    //so it will be like arr[0].burgerName and so on...
+    res.render("index", { burger_data: arr });
+  });
 });
 
 router.post('/create', function (req, res) {
-    res.redirect("/");
+  res.redirect("/");
 });
 
 router.put('/update', function (req, res) {
-    res.redirect('/');
+  res.redirect('/');
 });
 
 router.delete('/update', function (req, res) {
-    res.redirect('/');
+  res.redirect('/');
 });
 
 module.exports = router;
--- src/controllers/routes.js
+++ src/controllers/routes.js
@@ -10,16 +10,22 @@ router.get("/", function(req, res) {
   //Burger must be capitalized, because it is calling the variable inside of the burger model
   //use raw: true to display the data in an array format
   models.Burger.findAll({ raw: true}).then(found => {
-    console.log('length: ' + found.length);
+    // console.log('length: ' + found.length);
+    //create an empty array for objects to be pushed into, to be able to work with views engine
+    let arr = [];
     //use a for loop to loop through all of the potential answers in the array generated
     //from the query above
     for (i=0; i < found.length; i++) {
       //use JSON.stringify to covert the data into a workable object
-      console.log('this burger was not consumed: ' + JSON.stringify(found[i].burgerName));
-    }
+      // console.log('this burger was not consumed: ' + found[i]);
+      (arr).push(found[i]);
+    };
+    //arr will say [object object], that just means an object converted into a string
+    // console.log('this is arr: ' + arr);
+    //the PURPOSE OF arr is when the data is called in views it will be called in a each loop
+    //so it will be like arr[0].burgerName and so on...
+    res.render("index", { burger_data: arr });
   })
-
-  res.render("index");
 });
 
 router.post('/create', function(req, res) {
